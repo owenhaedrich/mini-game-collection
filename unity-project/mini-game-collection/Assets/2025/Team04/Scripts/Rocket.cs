c207583dfc3656bb7bbf57d7412c8420bc5e0cbe
Haedrich, Owen <000968723@mohawkcollege.ca>
2025-11-01 14:26:30 -0400

    Rockets that can be held and fired

--- unity-project/mini-game-collection/Assets/2025/Team04/Scripts/Rocket.cs
+++ unity-project/mini-game-collection/Assets/2025/Team04/Scripts/Rocket.cs
@@ -1,40 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
+using MiniGameCollection;
 using UnityEngine;
 
-public class Rocket : MonoBehaviour
+namespace MiniGameCollection.Games2025.Team04
 {
-    bool player2;
-    bool fired = true;
-    float rocketForce = 10f;
-    Vector2 up = Vector2.left;
-    public Transform centerLine;
-    Rigidbody2D rigidbody;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Rocket : MiniGameBehaviour
     {
-        if (player2)
+        public bool player2;
+        public bool fired = false;
+        public Vector3 moveToPosition;
+        float fireTimer = 0.3f;
+        float moveToSpeed = 4f;
+        float maxDistanceFromMoveTo = 0.1f;
+        float rocketForce = 10f;
+        float gravityForce = 7f;
+        Vector2 up = Vector2.left;
+        Rigidbody2D rigidbody;
+        CircleCollider2D collider;
+        Transform centerLine;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            up = Vector2.right;
-        }
-        rigidbody = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void FixedUpdate()
-    {
-        if (fired)
-        {
-            rigidbody.AddForce(up * rocketForce);
+            if (player2)
+            {
+                up = Vector2.right;
+            }
+            rigidbody = GetComponent<Rigidbody2D>();
+            collider = GetComponent<CircleCollider2D>();
+            centerLine = GameObject.Find("CenterLine").transform;
+            moveToPosition = transform.position;
         }
 
-        float distanceFromCenter = centerLine.position.x - transform.position.x;
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void FixedUpdate()
+        {
+            if (fired)
+            {
+                collider.isTrigger = false;
+                float distanceFromCenter = centerLine.position.x - transform.position.x;
+                Vector2 appliedGravity = Vector2.left * Mathf.Sign(distanceFromCenter) * gravityForce;
+                rigidbody.AddForce(appliedGravity);
+                if (fireTimer > 0f)
+                {
+                    fireTimer -= Time.fixedDeltaTime;
+                }
+                else
+                {
+                    rigidbody.AddForce(up * rocketForce);
+                }
+            }
+            else
+            {
+                rigidbody.transform.position = MoveToController(moveToPosition, moveToSpeed);
+            }
+        }
+
+        Vector3 MoveToController(Vector3 targetPosition, float desiredSpeed)
+        {
+            Vector3 nextPosition = Vector3.MoveTowards(rigidbody.transform.position, targetPosition, desiredSpeed * Time.fixedDeltaTime);
+            float distanceToTarget = Vector3.Distance(nextPosition, targetPosition);
+            if (distanceToTarget < maxDistanceFromMoveTo)
+            {
+                nextPosition = Vector3.MoveTowards(rigidbody.transform.position, targetPosition, distanceToTarget);
+            }
+
+            return nextPosition;
+        }
     }
 }
-
